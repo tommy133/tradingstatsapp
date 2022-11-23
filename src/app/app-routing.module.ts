4160269e9935990c35d2cda9c06471edd195eda9
Tomeu Estrany <tomestrany@gmail.com>
2022-11-23 09:02:30 +0100

    feat:dashboard component and routing activated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OperationComponent } from './operation/operation.component';
+import { ProjectionComponent } from './projection/projection.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'projections', component: ProjectionComponent },
+  { path: 'operations', component: OperationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectionComponent } from './projection/projection.component';
 import { OperationComponent } from './operation/operation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectionComponent,
-    OperationComponent
+    OperationComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
