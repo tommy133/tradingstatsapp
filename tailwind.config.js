2f286e3082a05841241a7b2a532e1621aebefe60
Tomeu <tomestrany@gmail.com>
2023-06-11 10:08:29 +0200

    config:tailwind config file added

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+plugin = require('tailwindcss/plugin');
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+}
+
