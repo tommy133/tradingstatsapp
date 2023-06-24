d374784247b6d9ed29bd442594fcd807e6147e11
Tomeu <tomestrany@gmail.com>
2023-06-24 20:43:49 +0200

    feat: add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { OperationLayoutComponent } from './layout/operation-layout/operation-layout.component';
 import { ProjectionLayoutComponent } from './layout/projection-layout/projection-layout.component';
-import { HomeComponent } from './modules/home/pages/home/home.component';
 import { OperationModule } from './modules/operation/operation.module';
 import { ProjectionModule } from './modules/projection/projection.module';
 import { SharedModule } from './shared/shared.module';
@@ -27,6 +28,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    CoreModule,
     SharedModule,
     ProjectionModule,
     OperationModule,
