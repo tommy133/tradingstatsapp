092ccfb2a8a5d5ae2103907b49970bc730545a8f
Tomeu <tomestrany@gmail.com>
2023-07-02 11:53:01 +0200

    feat: form controls added

--- src/app/modules/projection/pages/projection/projection-add/projection-add.component.ts
+++ src/app/modules/projection/pages/projection/projection-add/projection-add.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Symbol, SymbolService } from 'src/app/data/service/symbol.service';
+import { Status } from 'src/app/data/status';
+import { Timeframe } from 'src/app/data/timeframe';
 import { Projection } from '../../../model/projection';
 import { ProjectionService } from '../../../service/projection.service';
 
@@ -7,7 +12,32 @@ import { ProjectionService } from '../../../service/projection.service';
   templateUrl: './projection-add.component.html',
 })
 export class ProjectionAddComponent {
-  constructor(private projectionService: ProjectionService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private projectionService: ProjectionService,
+    private symbolService: SymbolService,
+  ) {}
+
+  isLoading: boolean = false;
+
+  symbols$: Observable<Symbol[]> = this.symbolService.getSymbols();
+  /* timeframe!: Timeframe
+  status!: Status */
+
+  symbol = this.formBuilder.control<Symbol | null>(null);
+  orderType = this.formBuilder.control<string | null>(null);
+  //chart
+  timeframe = this.formBuilder.control<Timeframe | null>(null);
+  status = this.formBuilder.control<Status | null>(null);
+  comment = this.formBuilder.control<string | null>(null);
+
+  addProjectionForm = this.formBuilder.group({
+    symbol: this.symbol,
+    orderType: this.orderType,
+    timeframe: this.timeframe,
+    status: this.status,
+    comment: this.comment,
+  });
 
   onAddProjection(projection: Projection): void {
     this.projectionService.addProjection(projection);
