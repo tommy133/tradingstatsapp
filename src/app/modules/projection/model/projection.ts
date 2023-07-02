3569ded8d0d4a259996332bfb421cf7d3eb96c15
Tomeu <tomestrany@gmail.com>
2023-07-02 10:18:55 +0200

    feature: projection data

--- src/app/modules/projection/model/projection.ts
+++ src/app/modules/projection/model/projection.ts
@@ -1,9 +1,12 @@
+import { Status } from 'src/app/data/status';
+import { Timeframe } from 'src/app/data/timeframe';
+
 export interface Projection {
   id?: number;
   name_sym?: string;
   updown?: boolean;
   date_proj?: Date;
-  name_tf?: string;
+  name_tf?: Timeframe;
   graph?: string;
-  name_st?: string;
+  name_st?: Status;
 }
