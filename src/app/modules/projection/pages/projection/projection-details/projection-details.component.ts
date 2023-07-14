cbc6c6e533f8297993bc1b65cd2697288a819615
Tomeu <tomestrany@gmail.com>
2023-07-15 01:53:33 +0200

    feat: mocked details

--- src/app/modules/projection/pages/projection/projection-details/projection-details.component.ts
+++ src/app/modules/projection/pages/projection/projection-details/projection-details.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { ToastService } from 'src/app/core/service/toast.service';
+import { ProjectionComment } from 'src/app/data/models/pcomment';
+import { ProjectionCommentService } from 'src/app/data/service/pcomment.service';
+import { Projection } from '../../../model/projection';
+import { ProjectionService } from '../../../service/projection.service';
 
 @Component({
   selector: 'app-projection-details',
-  templateUrl: './projection-details.component.html'
+  templateUrl: './projection-details.component.html',
 })
 export class ProjectionDetailsComponent implements OnInit {
+  projectionDetails$?: Observable<Projection>;
+  comments$?: Observable<ProjectionComment>;
+  isLoading: boolean = false;
+  errors: Array<string> = [];
 
-  constructor() { }
+  constructor(
+    private projectionService: ProjectionService,
+    private commentService: ProjectionCommentService,
+    private toastService: ToastService,
+    private activatedRoute: ActivatedRoute,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.projectionDetails$ = this.activatedRoute.params.pipe(
+      switchMap((params) => {
+        const id = params['id'];
+        return this.projectionService.getProjection(id);
+      }),
+    );
+
+    this.comments$ = this.activatedRoute.params.pipe(
+      switchMap((params) => {
+        const id = params['id'];
+        return this.commentService.getComment(id);
+      }),
+    );
   }
-
 }
