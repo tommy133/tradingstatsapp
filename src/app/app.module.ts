2935e4fec17cbac53f164f3de7bdc8f2258d48d8
Tomeu <tomestrany@gmail.com>
2023-07-15 18:25:11 +0200

    feat: add fileupload module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FileUploadModule } from 'ng2-file-upload/file-upload/file-upload.module';
+import { ToastPopupComponent } from './layout/main-layout/components/toast-popup/toast-popup.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { OperationLayoutComponent } from './layout/operation-layout/operation-layout.component';
 import { ProjectionLayoutComponent } from './layout/projection-layout/projection-layout.component';
@@ -13,7 +15,6 @@ import { HomeComponent } from './modules/home/pages/home/home.component';
 import { OperationModule } from './modules/operation/operation.module';
 import { ProjectionModule } from './modules/projection/projection.module';
 import { SharedModule } from './shared/shared.module';
-import { ToastPopupComponent } from './layout/main-layout/components/toast-popup/toast-popup.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { ToastPopupComponent } from './layout/main-layout/components/toast-popup
     SharedModule,
     ProjectionModule,
     OperationModule,
+    FileUploadModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
