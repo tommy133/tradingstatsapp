5bc32fb3bee8f6a47d208eca0188c74c31a2c1c8
Tomeu <tomestrany@gmail.com>
2023-07-20 18:36:49 +0200

    feat: download and display chart

--- src/app/modules/projection/pages/view-chart/view-chart.component.ts
+++ src/app/modules/projection/pages/view-chart/view-chart.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { FileService } from 'src/app/file.service';
 
 @Component({
   selector: 'app-view-chart',
   templateUrl: './view-chart.component.html',
 })
-export class ViewChartComponent {
-  constructor() {}
+export class ViewChartComponent implements OnInit {
+  imageUrl?: SafeUrl;
+  constructor(
+    private fileService: FileService,
+    private activatedRoute: ActivatedRoute,
+    private sanitizer: DomSanitizer,
+  ) {}
+
+  ngOnInit() {
+    const filename = this.activatedRoute.snapshot.queryParamMap.get('fileName');
+
+    if (filename) {
+      this.downloadFile(filename);
+    }
+  }
+
+  private downloadFile(filename: string) {
+    this.fileService.downloadFile(filename).subscribe(
+      (data) => {
+        const blob = new Blob([data], { type: 'image/png' });
+        this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(
+          URL.createObjectURL(blob),
+        );
+      },
+      (error) => console.error(error),
+    );
+  }
 }
