e23ec71657fab141b9a1bdd4b9cdad8125327ccd
Tomeu <tomestrany@gmail.com>
2023-07-27 13:52:55 +0200

    refactor: remove old filter

--- src/app/modules/projection/pages/projection/projection-list/projection-list.component.ts
+++ src/app/modules/projection/pages/projection/projection-list/projection-list.component.ts
@@ -1,13 +1,11 @@
 import { trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map, Observable, of, Subscription } from 'rxjs';
 import { FormService } from 'src/app/core/service/form.service';
 import {
   sidebarRightAnimationSlide,
   SidebarRightAnimationState,
 } from 'src/app/shared/utils/sidebar-right-animation';
-import { Projection } from '../../../model/projection';
 import { ProjectionService } from '../../../service/projection.service';
 
 @Component({
@@ -26,55 +24,19 @@ export class ProjectionListComponent {
     this.searchProjections$,
     ({ symbol }) => symbol.nameSymbol,
   );
-  private filterSubscription?: Subscription;
 
   sidebarRightAnimationState: SidebarRightAnimationState = 'out';
 
   constructor(
     private projectionService: ProjectionService,
     private formService: FormService,
-  ) {
-    this.filteredProjections$.subscribe(console.log);
-  }
+  ) {}
 
   ngOnInit() {
     this.projectionService.setRefetchInterval();
   }
 
-  public getProjections(): Observable<Projection[]> {
-    return this.projectionService.getProjections();
-  }
-
   public onDeleteProjection(projectionId: number): void {
     this.projectionService.deleteProjection(projectionId);
-  }
-  //TODO real time filter
-  public onFilterProjections(key: string): void {
-    const fieldContainsKey = (field: string) =>
-      field.toLowerCase().includes(key.toLowerCase());
-    const filterFn = (projection: Projection) => {
-      const {
-        symbol: { nameSymbol },
-        status: { nameStatus },
-      } = projection;
-
-      return (
-        fieldContainsKey(nameSymbol) ||
-        fieldContainsKey(nameStatus) ||
-        fieldContainsKey(projection.timeframe)
-      );
-    };
-
-    this.filterSubscription = this.projections$
-      ?.pipe(map((projections) => projections.filter(filterFn)))
-      .subscribe((filteredProjections) => {
-        this.projections$ = key
-          ? of(filteredProjections)
-          : this.getProjections();
-      });
-  }
-
-  ngOnDestroy() {
-    this.filterSubscription?.unsubscribe();
   }
 }
