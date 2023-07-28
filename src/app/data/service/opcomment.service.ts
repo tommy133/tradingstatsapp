a4e761c8afeb6a6df8662bd93f22c578123d1b5b
Tomeu <tomestrany@gmail.com>
2023-07-28 15:29:54 +0200

    feat: opcomment service

---
+++ src/app/data/service/opcomment.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { OperationComment } from '../models/opcomment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OperationCommentService {
+  private serverUrl = `${environment.apiBaseUrl}/opcomment`;
+
+  constructor(private http: HttpClient) {}
+
+  public getComments(): Observable<OperationComment[]> {
+    return this.http.get<OperationComment[]>(`${this.serverUrl}`);
+  }
+
+  public getComment(operationId: number): Observable<OperationComment> {
+    return this.http.get<OperationComment>(`${this.serverUrl}/${operationId}`);
+  }
+
+  public addComment(comment: OperationComment) {
+    return this.http.post(`${this.serverUrl}`, comment);
+  }
+
+  public updateComment(comment: OperationComment) {
+    return this.http.put(`${this.serverUrl}/${comment.id_opc}`, comment);
+  }
+
+  public deleteComment(operationId: number) {
+    return this.http.delete(`${this.serverUrl}/${operationId}`);
+  }
+}
