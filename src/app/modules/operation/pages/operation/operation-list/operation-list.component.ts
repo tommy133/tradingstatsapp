39b6c04a59681623d37695f9d93ea3b4cd96d2e1
Tomeu <tomestrany@gmail.com>
2023-07-28 10:48:11 +0200

    feat: operation list scaffolding

--- src/app/modules/operation/pages/operation/operation-list/operation-list.component.ts
+++ src/app/modules/operation/pages/operation/operation-list/operation-list.component.ts
@@ -1,12 +1,42 @@
+import { trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormService } from 'src/app/core/service/form.service';
+import {
+  SidebarRightAnimationState,
+  sidebarRightAnimationSlide,
+} from 'src/app/shared/utils/sidebar-right-animation';
+import { OperationService } from '../../../service/operation.service';
 
 @Component({
   selector: 'app-operation-list',
   templateUrl: './operation-list.component.html',
-  styleUrls: ['./operation-list.component.css'],
+  animations: [trigger('sidebarRightInOut', sidebarRightAnimationSlide)],
 })
 export class OperationListComponent implements OnInit {
-  constructor() {}
+  operations$ = this.operationService.projections$;
+  searchOperationsControl = new FormControl<string>('');
+  searchOperations$ = this.formService.applyDebounceOnSearch(
+    this.searchOperationsControl.valueChanges,
+  );
+  filteredOperations$ = this.formService.filterItems(
+    this.operations$,
+    this.searchOperations$,
+    ({ symbol }) => symbol.nameSymbol,
+  );
 
-  ngOnInit(): void {}
+  sidebarRightAnimationState: SidebarRightAnimationState = 'out';
+
+  constructor(
+    private operationService: OperationService,
+    private formService: FormService,
+  ) {}
+
+  ngOnInit() {
+    this.operationService.setRefetchInterval();
+  }
+
+  public onDeleteOperation(operationId: number): void {
+    //this.operationService.deleteOperation(operationId);
+  }
 }
