13b532f542a605a674059a44f762e932a30d1afd
Tomeu <tomestrany@gmail.com>
2023-08-05 00:59:11 +0200

    feat: display account type and navigate to list by account type

--- src/app/layout/operation-layout/operation-layout.component.ts
+++ src/app/layout/operation-layout/operation-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OperationService } from 'src/app/modules/operation/service/operation.service';
 import { NavButton } from 'src/app/shared/utils/custom-types';
 
 @Component({
@@ -7,6 +9,10 @@ import { NavButton } from 'src/app/shared/utils/custom-types';
   styles: [],
 })
 export class OperationLayoutComponent {
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  private operationService = inject(OperationService);
+
   title: string = 'Trading Stats';
   buttons: NavButton[] = [
     {
@@ -18,6 +24,30 @@ export class OperationLayoutComponent {
       link: '/operations',
     },
   ];
+  account!: number;
 
-  constructor() {}
+  ngOnInit() {
+    const queryParams = { ...this.activatedRoute.snapshot.queryParams };
+    this.router.navigate(
+      [],
+
+      { queryParams: { ...queryParams, account: 1 } },
+    );
+    this.account = 1;
+  }
+
+  get accountType(): string {
+    return this.account === 1 ? 'Demo' : 'Live';
+  }
+
+  switchAccount() {
+    const queryParams = { ...this.activatedRoute.snapshot.queryParams };
+    this.router.navigate(
+      [],
+
+      { queryParams: { ...queryParams, account: this.account === 1 ? 2 : 1 } },
+    );
+    this.account = this.account === 1 ? 2 : 1;
+    this.operationService.refetch();
+  }
 }
