7819c5104baaae832058a1e63e71968c17b59137
Tomeu <tomestrany@gmail.com>
2023-08-05 13:40:58 +0200

    fix: redirect to details with account queryparam

--- src/app/modules/operation/pages/operation/operation-list/table-operation/table-operation.component.ts
+++ src/app/modules/operation/pages/operation/operation-list/table-operation/table-operation.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Operation } from 'src/app/modules/operation/model/operation';
 
 export interface TableColumn {
@@ -10,6 +11,8 @@ export interface TableColumn {
   templateUrl: './table-operation.component.html',
 })
 export class TableOperationComponent {
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
   @Input() rows!: Operation[];
   @Output() deleteEvent = new EventEmitter<number>();
 
@@ -25,6 +28,14 @@ export class TableOperationComponent {
 
   getPointsColorClass(points: number): string {
     return points > 0 ? 'text-green' : 'text-red';
+  }
+
+  goToDetails(operationId: number) {
+    this.router.navigate(['.', operationId], {
+      relativeTo: this.activatedRoute,
+      queryParams: this.activatedRoute.snapshot.queryParams,
+      queryParamsHandling: 'preserve',
+    });
   }
 
   deleteOperation(operationId: number) {
