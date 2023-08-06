7f099fdf167f0733db1443e5256ef949c64bf3fd
Tomeu <tomestrany@gmail.com>
2023-08-06 22:00:57 +0200

    feat: add viewchart id param and unmock card

--- src/app/modules/projection/pages/view-chart/view-chart.component.ts
+++ src/app/modules/projection/pages/view-chart/view-chart.component.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { ITradingViewWidget } from 'angular-tradingview-widget';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription, switchMap } from 'rxjs';
 import { ToastService } from 'src/app/core/service/toast.service';
 import { FileService } from 'src/app/file.service';
+import { Projection } from '../../model/projection';
+import { ProjectionService } from '../../service/projection.service';
 
 @Component({
   selector: 'app-view-chart',
   templateUrl: './view-chart.component.html',
 })
 export class ViewChartComponent implements OnInit {
+  projection$?: Observable<Projection>;
   fileSubscription?: Subscription;
   imageUrl?: SafeUrl;
   widgetConfig: ITradingViewWidget = {
@@ -18,6 +21,7 @@ export class ViewChartComponent implements OnInit {
     widgetType: 'widget',
   };
   constructor(
+    private projectionService: ProjectionService,
     private fileService: FileService,
     private activatedRoute: ActivatedRoute,
     private sanitizer: DomSanitizer,
@@ -25,6 +29,13 @@ export class ViewChartComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.projection$ = this.activatedRoute.params.pipe(
+      switchMap((params) => {
+        const id = params['id'];
+        return this.projectionService.getProjection(id);
+      }),
+    );
+
     const filename = this.activatedRoute.snapshot.queryParamMap.get('fileName');
 
     if (filename) {
