db1752d436027083683736b7b511db170b4dcd06
Tomeu <tomestrany@gmail.com>
2023-08-06 19:46:24 +0200

    refactor: apply grAy color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,10 @@ module.exports = {
     extend: {
       colors: {
         'dark': '#333',
-        'dark-grey': '#3A3A3A',
+        'dark-gray': '#3A3A3A',
         'blue': '#2dd4bf',
         'orange': '#FF9033',
-        'light-grey': '#D3D3D3',
+        'light-gray': '#D3D3D3',
         'green': '#32CD32',
         'dark-green': '#006400',
         'red-violet': '#c71558',
