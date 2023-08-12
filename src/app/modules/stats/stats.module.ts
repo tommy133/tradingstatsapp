40bf91a181732d8497cd21044e47beaa04584efc
Tomeu <tomestrany@gmail.com>
2023-08-13 00:24:52 +0200

    feat: view stats component and route

--- src/app/modules/stats/stats.module.ts
+++ src/app/modules/stats/stats.module.ts
@@ -3,11 +3,14 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ViewStatsComponent } from './pages/view-stats/view-stats.component';
+import { StatsRoutingModule } from './stats.routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [ViewStatsComponent],
   imports: [
     CommonModule,
+    StatsRoutingModule,
     HttpClientModule,
     SharedModule,
     HighchartsChartModule,
--- src/app/modules/stats/stats.routing.module.ts
+++ src/app/modules/stats/stats.routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewStatsComponent } from './pages/view-stats/view-stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'view-stats', pathMatch: 'full' },
+  {
+    path: 'view-stats',
+    component: ViewStatsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
