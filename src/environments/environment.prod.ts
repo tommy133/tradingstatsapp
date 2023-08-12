7e570c1997c72d0da45eef8484c309174e4595b4
Tomeu <tomestrany@gmail.com>
2023-08-12 18:29:44 +0200

    config: set local configuration environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: true,
+  production: false,
+  apiBaseUrl: 'https://app-94357af9-3925-43fa-8d1b-e7c475223036.cleverapps.io',
 };
