fe71bcfeb865ad09851f5d8eba4e7e2545910c38
Tomeu <tomestrany@gmail.com>
2023-08-13 12:03:55 +0200

    refactor: move highcharts into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HighchartsChartModule } from 'highcharts-angular';
 import { ToastPopupComponent } from './layout/main-layout/components/toast-popup/toast-popup.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { OperationLayoutComponent } from './layout/operation-layout/operation-layout.component';
 import { ProjectionLayoutComponent } from './layout/projection-layout/projection-layout.component';
+import { StatsLayoutComponent } from './layout/stats-layout/stats-layout.component';
 import { HomeComponent } from './modules/home/pages/home/home.component';
 import { OperationModule } from './modules/operation/operation.module';
 import { ProjectionModule } from './modules/projection/projection.module';
+import { StatsModule } from './modules/stats/stats.module';
 import { SharedModule } from './shared/shared.module';
-import { StatsLayoutComponent } from './layout/stats-layout/stats-layout.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +37,8 @@ import { StatsLayoutComponent } from './layout/stats-layout/stats-layout.compone
     SharedModule,
     ProjectionModule,
     OperationModule,
+    StatsModule,
+    HighchartsChartModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/modules/stats/stats.module.ts
+++ src/app/modules/stats/stats.module.ts
@@ -1,20 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { HighchartsChartModule } from 'highcharts-angular';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ViewStatsComponent } from './pages/view-stats/view-stats.component';
 import { StatsRoutingModule } from './stats.routing.module';
 
 @NgModule({
   declarations: [ViewStatsComponent],
-  imports: [
-    CommonModule,
-    StatsRoutingModule,
-    HttpClientModule,
-    SharedModule,
-    HighchartsChartModule,
-  ],
+  imports: [CommonModule, StatsRoutingModule, HttpClientModule, SharedModule],
   providers: [HttpClient],
 })
 export class StatsModule {}
