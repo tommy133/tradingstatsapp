cc3da7d43dd6259d261bb945c6cb26afc47f5796
Tomeu <tomestrany@gmail.com>
2023-08-19 10:18:48 +0200

    refactor: project content  and param style label

--- src/app/shared/components/label/label.component.ts
+++ src/app/shared/components/label/label.component.ts
@@ -2,19 +2,15 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-label',
-  template: `<label
-    [class]="
-      bgColor +
-      ' ' +
-      textColor +
-      ' inline-flex items-center justify-center rounded-md px-2 font-semibold text-sm'
-    "
-  >
+  template: `<label [ngClass]="bgColor + ' ' + textColor + ' ' + style">
     {{ text }}
+    <ng-content></ng-content>
   </label> `,
 })
 export class LabelComponent {
   @Input() text: string = '';
   @Input() bgColor: string = 'bg-light-gray';
   @Input() textColor: string = 'text-black';
+  @Input() style: string =
+    'inline-flex items-center justify-center rounded-md px-2 font-semibold text-sm';
 }
