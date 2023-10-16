51623c4da5f6cc06fa897b907df40741766424db
Tomeu <tomestrany@gmail.com>
2023-10-16 21:19:46 +0200

    remove hardcoded local config in file api call

--- src/app/file.service.ts
+++ src/app/file.service.ts
@@ -1,18 +1,20 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileService {
-  fileUploadUri = 'http://localhost:8080/file/upload';
+  fileUploadUri = `${environment.apiBaseUrl}/file/upload`;
+  fileDownloadUri = `${environment.apiBaseUrl}/file/download`;
 
   constructor(private http: HttpClient) {}
 
   public downloadFile(file: String) {
     let body = { filename: file };
 
-    return this.http.post('http://localhost:8080/file/download', body, {
+    return this.http.post(this.fileDownloadUri, body, {
       responseType: 'blob',
       headers: new HttpHeaders().append('Content-Type', 'application/json'),
     });
