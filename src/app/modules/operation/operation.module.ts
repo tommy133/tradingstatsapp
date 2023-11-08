da9092f3807919969061f63894276884be40b493
Tomeu <tomestrany@gmail.com>
2023-11-08 23:38:45 +0100

    remove ng2 file upload dependency

--- src/app/modules/operation/operation.module.ts
+++ src/app/modules/operation/operation.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FileUploadModule } from 'ng2-file-upload';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { OperationRoutingModule } from './operation-routing.module';
 import { OperationDetailsComponent } from './pages/operation/operation-details/operation-details.component';
@@ -23,7 +22,6 @@ import { ViewChartComponent } from './pages/view-chart/view-chart.component';
     OperationRoutingModule,
     HttpClientModule,
     SharedModule,
-    FileUploadModule,
   ],
   providers: [HttpClient],
 })
