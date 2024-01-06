68718d57e24b9ddae16a21e74ae4173e4f56cb7e
Tomeu <tomestrany@gmail.com>
2024-01-06 21:21:55 +0100

    component well sync queryparams

--- src/app/shared/components/trimester-selector/trimester-selector.component.ts
+++ src/app/shared/components/trimester-selector/trimester-selector.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { map } from 'rxjs';
 
 @Component({
@@ -8,16 +8,28 @@ import { map } from 'rxjs';
     *ngIf="selectedTrimesters$ | async as selectedTrimesters"
   >
     <div class="flex flex-row space-x-4">
-      <app-trimester-btn [selected]="selectedTrimesters[0]">
+      <app-trimester-btn
+        [selected]="selectedTrimesters[0]"
+        (selectedChange)="navigateSelectedTrimester('q1', $event)"
+      >
         Q1
       </app-trimester-btn>
-      <app-trimester-btn [selected]="selectedTrimesters[1]">
+      <app-trimester-btn
+        [selected]="selectedTrimesters[1]"
+        (selectedChange)="navigateSelectedTrimester('q2', $event)"
+      >
         Q2
       </app-trimester-btn>
-      <app-trimester-btn [selected]="selectedTrimesters[2]">
+      <app-trimester-btn
+        [selected]="selectedTrimesters[2]"
+        (selectedChange)="navigateSelectedTrimester('q3', $event)"
+      >
         Q3
       </app-trimester-btn>
-      <app-trimester-btn [selected]="selectedTrimesters[3]">
+      <app-trimester-btn
+        [selected]="selectedTrimesters[3]"
+        (selectedChange)="navigateSelectedTrimester('q4', $event)"
+      >
         Q4
       </app-trimester-btn>
     </div>
@@ -25,9 +37,8 @@ import { map } from 'rxjs';
 })
 export class TrimesterSelectorComponent {
   private activatedRoute = inject(ActivatedRoute);
-  constructor() {
-    this.selectedTrimesters$.subscribe(console.log);
-  }
+  private router = inject(Router);
+
   selectedTrimesters$ = this.activatedRoute.queryParams.pipe(
     map((quarters) => [
       quarters['q1'] === 'true',
@@ -36,4 +47,42 @@ export class TrimesterSelectorComponent {
       quarters['q4'] === 'true',
     ]),
   );
+
+  ngOnInit() {
+    const initialTrimester = this.getInitialTrimester();
+    const queryParams = this.activatedRoute.snapshot.queryParams;
+    if (
+      !queryParams['q1'] &&
+      !queryParams['q2'] &&
+      !queryParams['q3'] &&
+      !queryParams['q4']
+    ) {
+      this.navigateSelectedTrimester(initialTrimester, true);
+    }
+  }
+
+  navigateSelectedTrimester(selectedTrimester: string, state: boolean) {
+    const queryParams = {
+      ...this.activatedRoute.snapshot.queryParams,
+      [selectedTrimester]: state,
+    };
+
+    this.router.navigate(['.'], {
+      relativeTo: this.activatedRoute,
+      queryParams,
+    });
+  }
+
+  private getInitialTrimester() {
+    const currentMonth = new Date().getMonth();
+    if (currentMonth < 3) {
+      return 'q1';
+    } else if (currentMonth < 6) {
+      return 'q2';
+    } else if (currentMonth < 9) {
+      return 'q3';
+    } else {
+      return 'q4';
+    }
+  }
 }
