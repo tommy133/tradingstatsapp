f5699cdcefa3519dd886d0b6e0c1fddcad4f4b36
Tomeu <tomestrany@gmail.com>
2024-01-08 16:56:44 +0100

    navigate with year

--- src/app/shared/components/year-selector/year-selector.component.ts
+++ src/app/shared/components/year-selector/year-selector.component.ts
@@ -1,23 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-year-selector',
   templateUrl: './year-selector.component.html',
 })
-export class YearSelectorComponent {
-  year = 2023;
+export class YearSelectorComponent implements OnInit {
+  year: number = this.getInitialYear();
   minYear = 2000;
   maxYear = 2100;
+  @Output() onchangeEvent: EventEmitter<number> = new EventEmitter<number>();
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    const initialYear = this.activatedRoute.snapshot.queryParams['year'];
+    if (initialYear) this.year = initialYear;
+    else this.navigateOnDateChange();
+  }
+
+  private getInitialYear(): number {
+    const initialYear = new Date().getFullYear();
+    return initialYear;
+  }
+
+  private emitYearChange() {
+    this.onchangeEvent.emit(this.year);
+  }
 
   previousYear = () => {
     if (this.year > this.minYear) {
       this.year--;
+      this.navigateOnDateChange();
     }
   };
 
   nextYear = () => {
     if (this.year < this.maxYear) {
       this.year++;
+      this.navigateOnDateChange();
     }
   };
 
@@ -27,5 +48,13 @@ export class YearSelectorComponent {
 
   get isNextDisabled() {
     return this.year >= this.maxYear;
+  }
+
+  private navigateOnDateChange() {
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: { year: this.year },
+      queryParamsHandling: 'merge',
+    });
   }
 }
