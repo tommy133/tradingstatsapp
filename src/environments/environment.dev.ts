5dd40855b4f00d664b1adf3fd6fa4f4815f940cf
Tomeu <teco@trifork.com>
2024-04-24 23:30:02 +0200

    setup dev environment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,14 @@
+//To use localhost with production backend, you can use the following configuration:
+
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://192.168.1.196:8080',
+  firebaseConfig: {
+    apiKey: 'AIzaSyCCy4wR_lfkri2QGzRoPUcXTZtsFXV1Rhs',
+    authDomain: 'trading-stats-31429.firebaseapp.com',
+    projectId: 'trading-stats-31429',
+    storageBucket: 'trading-stats-31429.appspot.com',
+    messagingSenderId: '1015930729920',
+    appId: '1:1015930729920:web:b6e2a33ff5406ff856c425',
+  },
+};
