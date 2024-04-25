209fe29ee6fcc243f72ca5fdbb5aa1ef0a99e89e
Tomeu <tomestrany@gmail.com>
2024-04-25 18:07:09 +0200

    save default options localstorage

--- src/app/layout/operation-layout/operation-layout.component.ts
+++ src/app/layout/operation-layout/operation-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastService } from 'src/app/core/service/toast.service';
@@ -32,16 +32,26 @@ export class OperationLayoutComponent {
     },
   ];
 
-  account!: AccountType;
+  defaultAccount: WritableSignal<AccountType | null> = signal(
+    this.getDefaultAccount() ?? null,
+  );
+
   accountTypes: AccountType[] = ['Demo', 'Live', 'Backtest'];
+
   accountControl: FormControl<AccountType | null> =
-    this.formBuilder.control<AccountType>(
-      this.activatedRoute.snapshot.queryParams['account']
-        ? this.accountTypes[
-            parseInt(this.activatedRoute.snapshot.queryParams['account']) - 1
-          ]
-        : this.accountTypes[0],
-    );
+    this.formBuilder.control<AccountType>(this.getInitialAccount() ?? 'Demo');
+
+  get isDefaultAccount() {
+    const defaultAccount = this.defaultAccount();
+    if (defaultAccount) {
+      const currentAccount =
+        this.activatedRoute.snapshot.queryParams['account'];
+      const defaultAccountIndex = this.accountTypes.indexOf(defaultAccount) + 1;
+
+      return parseInt(currentAccount) === defaultAccountIndex;
+    }
+    return false;
+  }
 
   ngOnInit() {
     const account = this.activatedRoute.snapshot.queryParams['account'];
@@ -75,12 +85,69 @@ export class OperationLayoutComponent {
 
   redirectDefaultAccount() {
     const queryParams = { ...this.activatedRoute.snapshot.queryParams };
+    const accountNumber =
+      this.accountTypes.indexOf(this.defaultAccount() ?? 'Demo') + 1;
+
     this.router.navigate([], {
       queryParams: {
         ...queryParams,
-        account: 1,
+        account: accountNumber,
       },
     });
+  }
+
+  setDefaultAccount() {
+    const account = this.accountControl.value;
+    if (account) {
+      try {
+        localStorage.setItem('defaultAccount', account);
+        this.defaultAccount.set(account);
+        this.toastService.info({
+          message: `${account} account set as default`,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+  deleteDefaultAccount() {
+    const account = this.accountControl.value;
+    if (account) {
+      try {
+        localStorage.removeItem('defaultAccount');
+        this.defaultAccount.set(null);
+        this.toastService.info({
+          message: `${account} deleted as default`,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+  private getInitialAccount() {
+    const accountParams = this.activatedRoute.snapshot.queryParams['account'];
+    if (accountParams) {
+      const accountParamsId = parseInt(accountParams);
+      const index =
+        accountParamsId > 0 && accountParamsId <= this.accountTypes.length
+          ? accountParamsId - 1
+          : 0;
+
+      return this.accountTypes[index];
+    } else {
+      return this.defaultAccount();
+    }
+  }
+
+  private getDefaultAccount() {
+    try {
+      return localStorage.getItem('defaultAccount') as AccountType | null;
+    } catch (err) {
+      console.log(err);
+      return this.accountTypes[0];
+    }
   }
 
   ngOnDestroy() {
