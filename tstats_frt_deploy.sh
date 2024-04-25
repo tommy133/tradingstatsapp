9628304f485553b1cbfeee9980d474cc52a0f996
Tomeu <tomestrany@gmail.com>
2024-04-25 18:08:23 +0200

    remove superuser auth on docker commands

--- tstats_frt_deploy.sh
+++ tstats_frt_deploy.sh
@@ -9,12 +9,12 @@ docker push $DOCKER_IMAGE
 
 #recreate image/container in remote
 ssh -t tomeu@192.168.1.196 "
-    sudo docker stop $DOCKER_CONTAINER_NAME
-    sudo docker rm $DOCKER_CONTAINER_NAME
-    sudo docker rmi $DOCKER_IMAGE
+    docker stop $DOCKER_CONTAINER_NAME
+    docker rm $DOCKER_CONTAINER_NAME
+    docker rmi $DOCKER_IMAGE
 
-    sudo docker pull $DOCKER_IMAGE
-    sudo docker run -d -p 3000:80 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE"
+    docker pull $DOCKER_IMAGE
+    docker run -d -p 3000:80 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE"
 
 
 
