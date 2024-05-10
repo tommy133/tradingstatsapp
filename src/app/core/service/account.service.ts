d2853b1198bea086ecb506e2e6e56ab798b60f2b
Tomeu <tomestrany@gmail.com>
2024-05-10 22:13:49 +0200

    move account logic to service

---
+++ src/app/core/service/account.service.ts
@@ -0,0 +1,104 @@
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastService } from 'src/app/core/service/toast.service';
+import { AccountType } from 'src/app/shared/utils/custom-types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  private formBuilder = inject(FormBuilder);
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  private toastService = inject(ToastService);
+
+  defaultAccount: WritableSignal<AccountType | null> = signal(
+    this.getDefaultAccount() ?? null,
+  );
+
+  accountTypes: AccountType[] = ['Demo', 'Live', 'Backtest'];
+
+  accountControl: FormControl<AccountType | null> =
+    this.formBuilder.control<AccountType>(this.getInitialAccount() ?? 'Demo');
+
+  isDefaultAccount() {
+    const defaultAccount = this.defaultAccount();
+    if (defaultAccount) {
+      const currentAccount =
+        this.activatedRoute.snapshot.queryParams['account'];
+      const defaultAccountIndex = this.accountTypes.indexOf(defaultAccount) + 1;
+
+      return parseInt(currentAccount) === defaultAccountIndex;
+    }
+    return false;
+  }
+
+  switchAccount(account: AccountType) {
+    const queryParams = { ...this.activatedRoute.snapshot.queryParams };
+    const accountNumber = this.accountTypes.indexOf(account) + 1;
+    this.router.navigate([], {
+      queryParams: {
+        ...queryParams,
+        account: accountNumber,
+      },
+    });
+    this.toastService.info({
+      message: `Switched to ${account} account`,
+    });
+  }
+
+  setDefaultAccount() {
+    const account = this.accountControl.value;
+    if (account) {
+      try {
+        localStorage.setItem('defaultAccount', account);
+        this.defaultAccount.set(account);
+        this.toastService.info({
+          message: `${account} account set as default`,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+  deleteDefaultAccount() {
+    const account = this.accountControl.value;
+    if (account) {
+      try {
+        localStorage.removeItem('defaultAccount');
+        this.defaultAccount.set(null);
+        this.toastService.info({
+          message: `${account} deleted as default`,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+  private getInitialAccount() {
+    const accountParams = this.activatedRoute.snapshot.queryParams['account'];
+    if (accountParams) {
+      const accountParamsId = parseInt(accountParams);
+      const index =
+        accountParamsId > 0 && accountParamsId <= this.accountTypes.length
+          ? accountParamsId - 1
+          : 0;
+
+      return this.accountTypes[index];
+    } else {
+      return this.defaultAccount();
+    }
+  }
+
+  private getDefaultAccount() {
+    try {
+      return localStorage.getItem('defaultAccount') as AccountType | null;
+    } catch (err) {
+      console.log(err);
+      return this.accountTypes[0];
+    }
+  }
+}
--- src/app/layout/operation-layout/operation-layout.component.ts
+++ src/app/layout/operation-layout/operation-layout.component.ts
@@ -1,20 +1,16 @@
-import { Component, inject, signal, WritableSignal } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ToastService } from 'src/app/core/service/toast.service';
+import { Component, inject } from '@angular/core';
+import { AccountService } from 'src/app/core/service/account.service';
 import { OperationService } from 'src/app/modules/operation/service/operation.service';
-import { AccountType, NavButton } from 'src/app/shared/utils/custom-types';
+import { NavButton } from 'src/app/shared/utils/custom-types';
 
 @Component({
   selector: 'app-operation-layout',
   templateUrl: './operation-layout.component.html',
 })
 export class OperationLayoutComponent {
-  private router = inject(Router);
-  private activatedRoute = inject(ActivatedRoute);
   private operationService = inject(OperationService);
-  private toastService = inject(ToastService);
-  private formBuilder = inject(FormBuilder);
+
+  accountService = inject(AccountService);
 
   title: string = 'Trading Stats';
   buttons: NavButton[] = [
@@ -32,122 +28,18 @@ export class OperationLayoutComponent {
     },
   ];
 
-  defaultAccount: WritableSignal<AccountType | null> = signal(
-    this.getDefaultAccount() ?? null,
-  );
+  accountTypes = this.accountService.accountTypes;
 
-  accountTypes: AccountType[] = ['Demo', 'Live', 'Backtest'];
-
-  accountControl: FormControl<AccountType | null> =
-    this.formBuilder.control<AccountType>(this.getInitialAccount() ?? 'Demo');
-
-  get isDefaultAccount() {
-    const defaultAccount = this.defaultAccount();
-    if (defaultAccount) {
-      const currentAccount =
-        this.activatedRoute.snapshot.queryParams['account'];
-      const defaultAccountIndex = this.accountTypes.indexOf(defaultAccount) + 1;
-
-      return parseInt(currentAccount) === defaultAccountIndex;
-    }
-    return false;
-  }
-
-  ngOnInit() {
-    const account = this.activatedRoute.snapshot.queryParams['account'];
-    if (!account) {
-      this.redirectDefaultAccount();
-    }
-  }
-
-  private accountSubscription = this.accountControl.valueChanges.subscribe(
-    (account) => {
+  private accountSubscription =
+    this.accountService.accountControl.valueChanges.subscribe((account) => {
       if (account) {
-        this.switchAccount(account);
+        this.accountService.switchAccount(account);
         this.operationService.refetch();
       }
-    },
-  );
-
-  switchAccount(account: AccountType) {
-    const queryParams = { ...this.activatedRoute.snapshot.queryParams };
-    const accountNumber = this.accountTypes.indexOf(account) + 1;
-    this.router.navigate([], {
-      queryParams: {
-        ...queryParams,
-        account: accountNumber,
-      },
     });
-    this.toastService.info({
-      message: `Switched to ${account} account`,
-    });
-  }
 
-  redirectDefaultAccount() {
-    const queryParams = { ...this.activatedRoute.snapshot.queryParams };
-    const accountNumber =
-      this.accountTypes.indexOf(this.defaultAccount() ?? 'Demo') + 1;
-
-    this.router.navigate([], {
-      queryParams: {
-        ...queryParams,
-        account: accountNumber,
-      },
-    });
-  }
-
-  setDefaultAccount() {
-    const account = this.accountControl.value;
-    if (account) {
-      try {
-        localStorage.setItem('defaultAccount', account);
-        this.defaultAccount.set(account);
-        this.toastService.info({
-          message: `${account} account set as default`,
-        });
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
-
-  deleteDefaultAccount() {
-    const account = this.accountControl.value;
-    if (account) {
-      try {
-        localStorage.removeItem('defaultAccount');
-        this.defaultAccount.set(null);
-        this.toastService.info({
-          message: `${account} deleted as default`,
-        });
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
-
-  private getInitialAccount() {
-    const accountParams = this.activatedRoute.snapshot.queryParams['account'];
-    if (accountParams) {
-      const accountParamsId = parseInt(accountParams);
-      const index =
-        accountParamsId > 0 && accountParamsId <= this.accountTypes.length
-          ? accountParamsId - 1
-          : 0;
-
-      return this.accountTypes[index];
-    } else {
-      return this.defaultAccount();
-    }
-  }
-
-  private getDefaultAccount() {
-    try {
-      return localStorage.getItem('defaultAccount') as AccountType | null;
-    } catch (err) {
-      console.log(err);
-      return this.accountTypes[0];
-    }
+  get isDefaultAccount() {
+    return this.accountService.isDefaultAccount();
   }
 
   ngOnDestroy() {
