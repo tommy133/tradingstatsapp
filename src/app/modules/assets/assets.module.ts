b958ea82d4a05861fe89c7bfe49050ac519e6239
Tomeu <tomestrany@gmail.com>
2024-05-13 23:59:44 +0200

    initial table

--- src/app/modules/assets/assets.module.ts
+++ src/app/modules/assets/assets.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { AssetsRoutingModule } from './assets-routing.module';
 import { AssetListComponent } from './pages/asset/asset-list/asset-list.component';
 import { AssetMutationComponent } from './pages/asset/asset-mutation/asset-mutation.component';
+import { TableAssetComponent } from './pages/asset/asset-list/table-asset/table-asset.component';
 
 @NgModule({
-  declarations: [AssetListComponent, AssetMutationComponent],
+  declarations: [AssetListComponent, AssetMutationComponent, TableAssetComponent],
   imports: [
     CommonModule,
     AssetsRoutingModule,
---
+++ src/app/modules/assets/pages/asset/asset-list/table-asset/table-asset.component.ts
@@ -0,0 +1,54 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Symbol } from 'src/app/data/models/symbol';
+import { navigatePreservingQueryParams } from 'src/app/shared/utils/shared-utils';
+
+interface TableColumn {
+  name: string;
+}
+
+@Component({
+  selector: 'app-table-asset',
+  templateUrl: './table-asset.component.html',
+})
+export class TableAssetComponent {
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+
+  @Input() rows!: Symbol[];
+  @Output() deleteEvent = new EventEmitter<Symbol>();
+
+  readonly commonCellStyle = 'p-4 text-center flex-1 whitespace-nowrap';
+
+  columns: TableColumn[] = [
+    { name: 'Symbol name' },
+    { name: 'Market' },
+    { name: 'Number trades' },
+    { name: 'Hit ratio' },
+    { name: 'Actions' },
+  ];
+
+  goToDetails(symbolId: number) {
+    navigatePreservingQueryParams(
+      ['.', symbolId],
+      this.router,
+      this.activatedRoute,
+    );
+  }
+
+  gotoEditAsset(symbolId: number, event: any) {
+    event.stopPropagation();
+    navigatePreservingQueryParams(
+      [`/assets/${symbolId}/edit`],
+      this.router,
+      this.activatedRoute,
+    );
+  }
+
+  deleteAsset(symbol: Symbol, event: any) {
+    event.stopPropagation();
+    if (confirm('Are you sure you want to delete this symbol?')) {
+      this.deleteEvent.emit(symbol);
+    }
+  }
+}
