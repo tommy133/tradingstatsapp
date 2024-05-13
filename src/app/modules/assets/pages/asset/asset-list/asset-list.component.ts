7074f9b08268f073f7e9aa7b722ea321c4247c53
Tomeu <tomestrany@gmail.com>
2024-05-13 22:18:51 +0200

    asset list structure

--- src/app/modules/assets/pages/asset/asset-list/asset-list.component.ts
+++ src/app/modules/assets/pages/asset/asset-list/asset-list.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-asset-list',
   templateUrl: './asset-list.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AssetListComponent {
-
+  goToAdd() {}
 }
