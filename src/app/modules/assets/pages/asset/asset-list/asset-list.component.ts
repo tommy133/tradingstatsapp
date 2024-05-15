d8a49a655b80d666a0b7a672e8163bd8c33ec302
Tomeu <tomestrany@gmail.com>
2024-05-15 21:36:25 +0200

    pull real data and displat in table

--- src/app/modules/assets/pages/asset/asset-list/asset-list.component.ts
+++ src/app/modules/assets/pages/asset/asset-list/asset-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { map, shareReplay } from 'rxjs';
+import { SymbolService } from 'src/app/data/service/symbol.service';
 import { navigatePreservingQueryParams } from 'src/app/shared/utils/shared-utils';
 
 @Component({
@@ -10,6 +12,10 @@ import { navigatePreservingQueryParams } from 'src/app/shared/utils/shared-utils
 export class AssetListComponent {
   private router = inject(Router);
   private activatedRoute = inject(ActivatedRoute);
+  private assetService = inject(SymbolService);
+
+  assets$ = this.assetService.getSymbols().pipe(shareReplay(1));
+  n_assets$ = this.assets$.pipe(map((ass) => ass.length));
 
   goToAdd() {
     navigatePreservingQueryParams(['add'], this.router, this.activatedRoute);
