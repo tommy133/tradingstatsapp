a1caa0c9171a309e87abf09c204a03b4e7ff2913
Tomeu <tomestrany@gmail.com>
2024-07-04 20:19:52 +0200

    fix arrow buuttons view chart

--- src/app/modules/operation/pages/operation/operation-mutation/operation-mutation.component.ts
+++ src/app/modules/operation/pages/operation/operation-mutation/operation-mutation.component.ts
@@ -89,11 +89,11 @@ export class OperationMutationComponent implements OnInit {
     Validators.required,
   );
   dateOpen = this.formBuilder.control<string | null>(
-    this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm', 'UTC'),
+    this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm'),
     Validators.required,
   );
   dateClose = this.formBuilder.control<string | null>(
-    this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm', 'UTC'),
+    this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm'),
   );
   timeframe = this.formBuilder.control<string | null>(
     'M1',
@@ -233,15 +233,11 @@ export class OperationMutationComponent implements OnInit {
     this.selectedSymbol = operationDetails.symbol.name_sym;
     this.orderType.setValue(updown);
     this.dateOpen.setValue(
-      this.datePipe.transform(new Date(dateOpen!), 'yyyy-MM-ddTHH:mm', 'UTC'),
+      this.datePipe.transform(new Date(dateOpen!), 'yyyy-MM-ddTHH:mm'),
     );
     this.dateClose.setValue(
       dateClose
-        ? this.datePipe.transform(
-            new Date(dateClose),
-            'yyyy-MM-ddTHH:mm',
-            'UTC',
-          )
+        ? this.datePipe.transform(new Date(dateClose), 'yyyy-MM-ddTHH:mm')
         : null,
     );
     this.graphFileName = graph!;
