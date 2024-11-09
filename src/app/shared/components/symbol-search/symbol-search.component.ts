b2ed6b30f35805c2b55e2e798d44017ee072b8f1
Tomeu <tomestrany@gmail.com>
2024-11-09 01:17:39 +0100

    symbol selector select with keyboard arrow

--- src/app/shared/components/symbol-search/symbol-search.component.ts
+++ src/app/shared/components/symbol-search/symbol-search.component.ts
@@ -1,9 +1,21 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  inject,
+  Input,
+  OnDestroy,
+  Output,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map, Observable } from 'rxjs';
+import { map, Observable, Subscription } from 'rxjs';
 import { FormService } from 'src/app/core/service/form.service';
 import { SymbolService } from 'src/app/data/service/symbol.service';
 import { Symbol } from 'src/app/modules/assets/model/symbol';
+
 @Component({
   selector: 'app-symbol-search',
   template: `
@@ -17,12 +29,13 @@ import { Symbol } from 'src/app/modules/assets/model/symbol';
       />
       <div
         *ngIf="showSymbolSuggestions"
-        class="absolute w-full mt-1 no-scrollbar max-h-[50vh] overflow-y-auto
-      bg-white rounded"
+        class="absolute w-full mt-1 no-scrollbar max-h-[50vh] overflow-y-auto bg-white rounded"
       >
-        <ng-container *ngFor="let symbol of filteredSymbolsByName$ | async">
+        <ng-container *ngFor="let symbol of filteredSymbols; let i = index">
           <div
+            #symbolItem
             class="border-b cursor-pointer hover:bg-gray-300 p-4"
+            [class.bg-gray-300]="i === selectedIndex"
             (click)="selectSymbol(symbol)"
           >
             {{ symbol.name_sym }}
@@ -32,37 +45,90 @@ import { Symbol } from 'src/app/modules/assets/model/symbol';
     </div>
   `,
 })
-export class SymbolSearchComponent {
+export class SymbolSearchComponent implements OnDestroy {
   private formService = inject(FormService);
   private symbolService = inject(SymbolService);
 
   @Input() placeholder: string = 'Search symbol';
   @Input() selected = '';
-  @Output() symbolSelected = new EventEmitter<any>();
+  @Output() symbolSelected = new EventEmitter<Symbol>();
 
   symbols$: Observable<Symbol[]> = this.symbolService.assets$;
-
   showSymbolSuggestions = false;
   searchSymbolsControl = new FormControl<string>('');
   searchSymbols$ = this.formService.applyDebounceOnSearch(
     this.searchSymbolsControl.valueChanges,
   );
+  filteredSymbols!: Symbol[];
+
   isSearchActive$ = this.searchSymbolsControl.valueChanges.pipe(
     map((search) => (search?.length ?? 0) > 0),
   );
-  filteredSymbolsByName$ = this.formService.filterItems(
-    this.symbols$,
-    this.searchSymbols$,
-    ({ name_sym }) => name_sym,
-  );
+  filteredSymbolsByNameSubs?: Subscription;
+  filteredSymbolsByName$ = this.formService
+    .filterItems(this.symbols$, this.searchSymbols$, ({ name_sym }) => name_sym)
+    .subscribe((filteredSymbols) => (this.filteredSymbols = filteredSymbols));
+
+  @ViewChildren('symbolItem', { read: ElementRef })
+  symbolItems!: QueryList<ElementRef>;
+  selectedIndex = -1; // Tracks the currently highlighted item index
 
   onShowSuggestions(show: boolean) {
     this.showSymbolSuggestions = show;
+    this.selectedIndex = -1; // Reset the selection index when suggestions are shown
   }
 
   selectSymbol(symbol: Symbol) {
     this.symbolSelected.emit(symbol);
     this.showSymbolSuggestions = false;
     this.selected = symbol.name_sym;
+    this.selectedIndex = -1; // Reset the selection index
+  }
+
+  // Handle arrow key navigation
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (this.showSymbolSuggestions) {
+      if (event.key === 'ArrowDown') {
+        event.preventDefault();
+        this.moveSelectionDown();
+      } else if (event.key === 'ArrowUp') {
+        event.preventDefault();
+        this.moveSelectionUp();
+      } else if (event.key === 'Enter' && this.selectedIndex > -1) {
+        event.preventDefault();
+        const symbol = this.filteredSymbols[this.selectedIndex];
+        if (symbol) {
+          this.selectSymbol(symbol);
+        }
+      }
+    }
+  }
+
+  moveSelectionDown() {
+    this.selectedIndex++;
+    this.scrollIntoView();
+  }
+
+  moveSelectionUp() {
+    if (this.selectedIndex > 0) {
+      this.selectedIndex--;
+      this.scrollIntoView();
+    }
+  }
+
+  scrollIntoView() {
+    const currentItem = this.symbolItems?.get(this.selectedIndex);
+    if (currentItem) {
+      currentItem.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.filteredSymbolsByNameSubs)
+      this.filteredSymbolsByName$.unsubscribe();
   }
 }
